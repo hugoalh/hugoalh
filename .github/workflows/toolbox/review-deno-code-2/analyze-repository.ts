58a4feca27c07080e1c75b0c21920dea6597c2f8
hugoalh <32359235+hugoalh@users.noreply.github.com>
2024-04-07 13:14:48 +0800

    Update workflow

--- .github/workflows/toolbox/review-deno-code-2/analyze-repository.ts
+++ .github/workflows/toolbox/review-deno-code-2/analyze-repository.ts
@@ -2,8 +2,12 @@ import { setOutputs } from "https://raw.githubusercontent.com/hugoalh-studio/git
 import { getDenoFilesPath, validateDenoResources } from "../deno-utility.ts";
 await validateDenoResources();
 const filesPath: string[] = await getDenoFilesPath();
-const benchmarkDispatch: boolean = filesPath.filter(/(?:^|\/|\\|\.|_)bench\.tsx?$/.test).length > 0;
-const testDispatch: boolean = filesPath.filter(/(?:^|\/|\\|\.|_)test\.tsx?$/.test).length > 0;
+const benchmarkDispatch: boolean = filesPath.filter((path: string): boolean => {
+	return /(?:^|\/|\\|\.|_)bench\.tsx?$/.test(path);
+}).length > 0;
+const testDispatch: boolean = filesPath.filter((path: string): boolean => {
+	return /(?:^|\/|\\|\.|_)test\.tsx?$/.test(path);
+}).length > 0;
 setOutputs({
 	benchmark_dispatch: String(benchmarkDispatch),
 	files: JSON.stringify(filesPath),
